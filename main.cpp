96bb0debd3b818652516d0f6d816422260e68a59
Diwa Tomy <diwatomy04@gmail.com>
2025-02-13 17:20:24 +0530

    edit

--- main.cpp
+++ main.cpp
@@ -2,5 +2,14 @@
 #include "encryption.h"
 using namespace std;
 int main(){
+    string filename;
+    char mode;
+    cout << "Enter the filename: ";
+    getline(cin >> ws,filename);
+    cout << "Encrypt (e) or Decrypt (d)?";
+    cin >> mode;
+    if(mode=="e" || mode=="E"){
+        
+    } 
     return 0;
 }
