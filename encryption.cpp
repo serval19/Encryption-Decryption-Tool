097d0cad21b36ad04b48ca59b49744dd7f4ed7fe
Diwa Tomy <144988434+serval19@users.noreply.github.com>
2025-02-15 22:15:35 +0530

    edits

---
+++ encryption.cpp
@@ -0,0 +1,6 @@
+#include "encryption.h"
+#include <fstream>
+#include <cctype>
+bool encryptFile(const string& filename,bool encrypt){
+    
+}
--- main.cpp
+++ main.cpp
@@ -8,8 +8,26 @@ int main(){
     getline(cin >> ws,filename);
     cout << "Encrypt (e) or Decrypt (d)?";
     cin >> mode;
-    if(mode=="e" || mode=="E"){
+    if(mode=='e' || mode=='E'){
+        if(encryptFile(filename, true)){
+            cout << "Files encrypted successfully" << endl;
+        }
+        else{
+            cerr << "Error: Unable to perform encryption" << endl;
+        }
         
     } 
+    else if(mode=='d' || mode == 'D'){
+        if(encryptFile(filename, false)){
+            cout << "Decryption completed successfully" << endl;
+        }
+        else{
+            cerr << "Error: Unable to perform decryption" << endl;
+        }
+    }
+    else{
+        cerr << "Error: Invalid mode selection. Use 'e' for encryption or 'd' for decryption" << endl;
+
+    }
     return 0;
 }
