be03abfec3e7a16ebe0acd83f656a2d67c89db2a
Diwa Tomy <144988434+serval19@users.noreply.github.com>
2025-02-16 22:04:09 +0530

    completed

--- encryption.cpp
+++ encryption.cpp
@@ -1,6 +1,42 @@
 #include "encryption.h"
 #include <fstream>
 #include <cctype>
+using namespace std;
+
+bool performCeaserCipher(string& content, bool encrypt){
+    int shift= encrypt ? 3 : -3;
+    for (char& ch:content){
+        if(isalpha(ch)) {
+            // cc header file is used above
+            char base =isupper(ch) ? 'A' : 'a';
+            ch= static_cast<char>((ch - base + shift + 26)%26 + base);
+        }
+    }
+    return true;
+}
+
 bool encryptFile(const string& filename,bool encrypt){
+    // open input file
+    ifstream inFile(filename);
+    if(!inFile){
+        return false;
+    }
+    // read the file content
+    string content((istreambuf_iterator<char>(inFile)),{});
+    inFile.close();
+    if (performCeaserCipher(content,encrypt)){
+         // create an output file and write the modified content
+         ofstream outFile(encrypt ? "encrypted_"+filename : "decrypted_"+filename);
+         if(!outFile){
+             return false;
+         }
+         outFile << content;
+         outFile.close();
+         return true;
+
+    }
+       
+
     
+
 }
